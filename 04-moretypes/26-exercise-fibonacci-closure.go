32d0739ded4d7534f5510997d6019f33646fb4d9
Artem Basalaev <basa62.1996@gmail.com>
2024-10-20 20:43:05 +0200

    Add MoreTypes 24-26

---
+++ 04-moretypes/26-exercise-fibonacci-closure.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	res, a, b := 0, 0, 1
+	return func() int {
+		res, a, b = a, b, a+b
+		return res
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
